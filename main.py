06c09c89d14025bd2305d937677d1da4a077cf21
ref01l <salushkin.ds@students.dvfu.ru>
2021-01-14 17:58:13 +1000

    Update adder and drawer

--- main.py
+++ main.py
@@ -3,13 +3,24 @@ import itertools
 import networkx as nx
 import numpy.random as rnd
 import matplotlib.pyplot as plt
+from typing import Union
 
 
-def add_edge(f_item, s_item, graph=None):
+def add_edge(f_item: str, s_item: str, graph=Union[nx.Graph, nx.DiGraph]):
     graph.add_edge(f_item, s_item)
     graph.add_edge(s_item, f_item)
 
 
-if __name__ == '__main___':
+def draw(graph=Union[nx.Graph, nx.DiGraph]):
+    nx.draw_circular(
+        graph,
+        node_color='red',
+        node_size=1000,
+        with_labels=True
+    )
+
+    plt.show()
+
+
+if __name__ == '__main__':
     graph = nx.Graph()
-    add_edge('A', 'B', graph=graph)
